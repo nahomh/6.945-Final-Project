9a944261f4f67a39e1b4023a096b87e1d075a587
Malcom Gilbert <malcomgilbert@gmail.com>
2014-04-27 16:37:38 -0400

    fix load, comment out broken code

--- generics.scm
+++ generics.scm
@@ -3,12 +3,12 @@
 
 ;Constructiong Notes 
 
-(define (notes:create type . notes)
-	(generic:notes-construct type notes))
+; (define (notes:create type . notes)
+; 	(generic:notes-construct type notes))
 
-(define generic:notes-construct
-	(make-generic-operator 2 'create-notes))
+; (define generic:notes-construct
+; 	(make-generic-operator 2 'create-notes))
 
-(defhandler generic:sequence-construct
-	(lambda (type notes) notes)
-	(is-exactly list?) list?)
+; (defhandler generic:sequence-construct
+; 	(lambda (type notes) notes)
+; 	(is-exactly list?) list?)
--- load.scm
+++ load.scm
@@ -1,17 +1,17 @@
 (load "util")
 ; Load file
 
+;load all the necessary files for different operations
+(load "ghelper.scm")
+(load "generics.scm")
+(load "amb.scm")
 
 ; load all the logic files for the enviornment
-(cd "/logic")
+(cd "./logic")
 (load "chord.scm")
 (load "internal.scm")
 (load "note.scm")
 (load "operation.scm")
-
-;load all the necessary files for different operations
 (cd "..")
-(load "ghelper.scm")
-(load "generics.scm")
-(load "amb.scm")
+
 ; (load "tests.scm")
