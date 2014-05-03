00d4e7028eeda1bc9608fc3541312e11c7e8d4dd
Nahom Workie <nahomh@ubuntu.(none)>
2014-05-03 16:33:54 -0400

    get-cent value

--- logic/interval.scm
+++ logic/interval.scm
@@ -4,7 +4,38 @@
 ;intervals
 
 ;create a new interval with the starting note, ending note.
+
+;make the associations
+
+(define interval-names
+	(make-eq-hash-table))
+
+(define (interval-associations)
+	(hash-table/put! interval-names 0 "P1")
+	(hash-table/put! interval-names 1 "m2")
+	(hash-table/put! interval-names 2 "M2")
+	(hash-table/put! interval-names 3 "m3")
+	(hash-table/put! interval-names 4 "M3")
+	(hash-table/put! interval-names 5 "P4")
+	(hash-table/put! interval-names 7 "P5")
+	(hash-table/put! interval-names 8 "m6")
+	(hash-table/put! interval-names 9 "M6")
+	(hash-table/put! interval-names 10 "m7")
+	(hash-table/put! interval-names 11 "M7")
+	(hash-table/put! interval-names 12 "P8")
+	)
+	
+	
+
+
+
+
+
+
 (define (interval #!optional start-note end-note)
-	(define-cell start-note)
+	(define-cell interval)
+
+
+
 	(eq-put start-note 'type 'note)
 	(eq-put start-note 'data ')
--- logic/note.scm
+++ logic/note.scm
@@ -24,3 +24,52 @@
 ; second/third/fourth… = flat/sharp
 ; last = octave
 
+
+;default case C4--> 0
+
+(define C4 0)
+(define semitone 100)
+
+(define (get-ascii note)
+	(if (string? note)
+		(char->ascii (string-ref note 0)))
+	(if (char? note)
+		(char->ascii note))
+)
+;check if it matches middle c
+(define (is-middle? pitch octave accent)
+	(and (char=? pitch #\C) (= octave 4)) (= 0 (cadr accent)))
+
+;get added octave count
+(define (cent-octave-count octave)
+	(* (- octave 4) 1200))
+;TODO error check
+
+;get accent-value 
+(define (get-accent-count accent)
+	(let ((semi-count 100)
+		(accent-number (cadr accent))
+		(accent-type (car accent)))
+	(cond ((string=? accent-type "#") (* semi-count 1))
+		((string=? accent-type "b") (* semi-count -1)))
+	(* semi-count accent-number))
+
+
+(define (get-cent note)
+	(let (pitch (eq-get! note 'pitch))
+		(octave (eq-get! note 'octave))
+		(accent (eq-get! note 'accent))
+		(count 0))
+	;get the ascii value for the note
+	(if (is-middle? pitch octave accent) C4)
+	(cond ((char=? pitch #\B) (- count semitone))
+		((char=? pitch #\D) (- count (* 2 semitone)))
+		((char=? pitch #\A) (- count (* 3 semitone)))
+		((char=? pitch #\E) (- count (* 4 semitone)))
+		((char=? pitch #F) (- count (* 5 semitone)))
+		((char=? pitch #G) (- count (* 7 semitone))))
+	(+ count (cent-octave-count octave))
+	(+ count (get-accent-count accent)))
+
+
+
