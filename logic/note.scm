f4b4b716697799bef8a6eb8091a2d2b0bf101491
Malcom Gilbert <malcomgilbert@gmail.com>
2014-05-03 15:39:12 -0400

    make tests run

--- logic/note.scm
+++ logic/note.scm
@@ -28,10 +28,10 @@
 
 (define (empty-note)
   (define-cell note)
-  (eq-put! note 'pitch 'C)
+  (eq-put! note 'pitch #\C)
   (eq-put! note 'duration 0.25)
   (eq-put! note 'octave 4)
-  (eq-put! note 'accent '())
+  (eq-put! note 'accent '()))
 
 
 (define (create-note pitch-string duration)
@@ -41,55 +41,25 @@
       (eq-put! new-note 'pitch (get-pitch pitch-string))
       (eq-put! new-note 'duration duration)
       (eq-put! new-note 'accent (get-accent pitch-string))
-      (eq-put! new-note 'octave (get-octave pitch-string))
+      (eq-put! new-note 'octave (get-octave-num pitch-string))
       new-note)))
-
-(define (get-pitch pitch-string)
-  (string-ref pitch-string 0))
-
-(define (get-accent pitch-string)
-  (let* 
-    ((pitch-substr (string-tail pitch-string 1))
-      (num-sharps (string-count pitch-substr #/#)) 
-     (num-flats (string-count pitch-substr #/b))
-     (difference (- num-sharps num-flats)))
-    (if (> 0 difference)
-      (cons "#" difference)
-      (cons "b" difference))))
-
-
-(define (get-octave pitch-string)
-  (let* (
-    (last-flat (string-search-backward pitch-string "b"))
-    (last-sharp (string-search-backward pitch-string "#")))
-
-    (cond
-      ((and (eq? last-flat #f) (eq? last-sharp #f))
-        (string-tail pitch-string 1))
-      ((eq? last-flat #f)
-        (string-tail pitch-string last-sharp))
-      ((eq? last-sharp #f)
-        (string-tail pitch-string last-flat))
-      (else
-        (string-tail pitch-string (max last-flat last-sharp))))))
-
 
 (define (add note summand)
   (make-generic-operator 2))
 
-(defhandler add
-  (lambda (note octave)
-    (let ((current-octave (eq-get note 'octave)))
-      (eq-put!
-        note
-        'octave
-        'data
-        (+ octave current-octave)))
-    note)
-  note? octave?)
+; (defhandler add
+;   (lambda (note octave)
+;     (let ((current-octave (eq-get note 'octave)))
+;       (eq-put!
+;         note
+;         'octave
+;         'data
+;         (+ octave current-octave)))
+;     note)
+;   note? octave?)
 
-(define (compare-notes note1 note2)
-  (< (get-cent note1) (get-cent note2)))
+; (define (compare-notes note1 note2)
+;   (< (get-cent note1) (get-cent note2)))
 
-(define (sort-notes notes)
-  (sort notes compare-notes))
+; (define (sort-notes notes)
+;   (sort notes compare-notes))
