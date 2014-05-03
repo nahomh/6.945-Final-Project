83ae98fb5fb796d251c814b26b3e74cee3ce7e17
Malcom Gilbert <malcomgilbert@gmail.com>
2014-05-03 16:24:54 -0400

    tests for pitch operations

--- logic/pitch.scm
+++ logic/pitch.scm
@@ -1,24 +1,31 @@
+(define (string-count str pattern)
+  (if (equal? str "")
+    0
+    (length (string-search-all pattern str))))
+
+
 (define (get-pitch pitch-string)
-  (string-ref pitch-string 0))
+  (string-ref (string-upcase pitch-string) 0))
 
 (define (get-accent pitch-string)
   (let* 
     (
      (pitch-substr (string-tail pitch-string 1))
-     (num-sharps (string-count pitch-substr #\#)) 
-     (num-flats (string-count pitch-substr #\b))
+     (num-sharps (string-count pitch-substr "#")) 
+     (num-flats (string-count pitch-substr "b"))
      (difference (- num-sharps num-flats))
     )
-    (if (> 0 difference)
-      (cons "#" difference)
-      (cons "b" difference))))
+    (if (> difference 0)
+      (list "#" (abs difference))
+      (list "b" (abs difference)))))
 
 
 (define (get-octave pitch-string)
   (let* (
-    (last-flat (string-search-backward pitch-string "b"))
-    (last-sharp (string-search-backward pitch-string "#")))
-
+    (last-flat (string-search-backward "b" pitch-string))
+    (last-sharp (string-search-backward "#" pitch-string)))
+    ; (displaym "last-flat" last-flat)
+    ; (displaym "last-sharp" last-sharp)
     (cond
       ((and (eq? last-flat #f) (eq? last-sharp #f))
         (string-tail pitch-string 1))
@@ -30,5 +37,7 @@
         (string-tail pitch-string (max last-flat last-sharp))))))
 
 (define (get-octave-num pitch-string)
-  (string->number (get-octave pitch-string))
+  (let ((octave-str (get-octave pitch-string)))
+    (if (equal? "" octave-str) 
+      4 (string->number octave-str)))
 )
