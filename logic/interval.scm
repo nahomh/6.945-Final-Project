241ba66aa479e5e4ab910ab073b24132c962ec1e
Nahom Workie <nahomh@ubuntu.(none)>
2014-05-03 12:06:12 -0400

    intervals

--- logic/interval.scm
+++ logic/interval.scm
@@ -1,3 +1,10 @@
 ;representation for intervals in music
 
-(define-cell interval)
+
+;intervals
+
+;create a new interval with the starting note, ending note.
+(define (interval #!optional start-note end-note)
+	(define-cell start-note)
+	(eq-put start-note 'type 'note)
+	(eq-put start-note 'data ')
