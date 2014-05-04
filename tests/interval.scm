5ea5d551b4ac671774abe3d1a349a2bffed8a5f6
Nahom Workie <nahomh@ubuntu.(none)>
2014-05-04 12:51:56 -0400

    added intervals

---
+++ tests/interval.scm
@@ -0,0 +1,8 @@
+; (define interval-test-suite (test-suite-wrapper "Interval Operations Suite"))
+; (define intest (test interval-test-suite))
+; ; ()
+
+
+; (intest
+;   (= (cent-octave-count 4) 0)
+;   "should return 0 because there is no octave shift")
