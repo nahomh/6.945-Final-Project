268d8db27e7b36570df1d47a9c0429efc5af54a6
Malcom Gilbert <malcomgilbert@gmail.com>
2014-05-04 15:46:00 -0400

    rm unused files

--- logic/interval.scm
+++ logic/interval.scm
@@ -6,7 +6,16 @@
 ;create a new interval with the starting note, ending note.
 
 ; make the associations
+; Logic for intervals in a music representations. 
 
+; Here we define the logic for intervals. 
+
+; Specfication examples:
+; (interval c g) → (‘interval c4 g4 P5)
+; (interval b3 d3) → (‘interval b3 d3 m3)
+; (interval c e) → (‘interval c4 e4 M3)
+; (interval 2) → number of semitones
+; (interval “P5”) → name of interval
 
 (define interval-names
 	(make-eq-hash-table))
