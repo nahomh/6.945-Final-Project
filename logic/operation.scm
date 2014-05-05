e903db4d5aca81d1097a907f82c63da89916468b
Nahom Workie <nahomh@ubuntu.(none)>
2014-05-05 14:53:43 -0400

    transpose almost complete

--- logic/operation.scm
+++ logic/operation.scm
@@ -8,36 +8,50 @@
 
 ;interval
 
-;(define transpose 
-;	(make-generic-operator 2))
-;(defhandler transpose ())
+(define transpose 
+	(make-generic-operator 2))
+(defhandler transpose ())
 
 
-; (define transpose 
-; 	(make-generic-operator 2))
+(define transpose 
+	(make-generic-operator 2))
+
+(defhandler transpose
+  transpose-note note? integer?)
 
 ; (defhandler transpose
-;   transpose-note note? integer?)
+;   transpose-chord chord? integer?)
 
-; ; (defhandler transpose
-; ;   transpose-chord chord? integer?)
+; (defhandler transpose
+;   transpose-piece piece? integer?)
 
-; ; (defhandler transpose
-; ;   transpose-piece piece? integer?)
+(define (update-octave semitones octave)
+	(displaym "update-ocatave-semitones" semitones)
+	(displaym "update-octave-num" octave)
+	(let (
+		(semitones-count (modulo semitones 12))
+		(octave-count (quotient semitones 12))
+		)
+	(if (> semitones 12) (list semitones-count octave-count) 
+		(list semitones 0))
+	)
+)
 
-; (define (update-octave semitones octave)
-; 	(displaym "update-ocatave-semitones" semitones)
-; 	(displaym "update-octave-num" octave)
-; 	(let (
-; 		(semitones-count (modulo semitones 12))
-; 		(octave-count (quotient semitones 12))
-; 		)
-; 	(if (> semitones 12) (list semitones-count octave-count) 
-; 		(list semitones))
-; 	)
-; )
+(define (get-note note-val)
+	(displaym "get-note-value" note-val)
+	(displaym "note-val" (integer? note-val))
+	(displaym "hash-table-val" (hash-table/get note-values note-val 0))
+	(cond
+		((= note-val -200) (list #\A "#" 1))
+		((= note-val 300) (list #\D "#" 1))
+		((= note-val 600) (list #\F "#" 1))
+		((= note-val 800) (list #\G "#" 1))
+		((= note-val 900) (list #\G "#" 2))
+		(else (hash-table/get note-values note-val 0))
+		)
+)
 
-
+;;Todo: Need to create the actual note. Update functions with side-cases work. 
 
 (define (transpose-note note semitones)
 	(let (
@@ -46,23 +60,20 @@
 		)
 		(displaym "note-value" note-value)
 		(displaym "note-octave" note-octave)
-		(displaym "update-octave-function" values)
 		(let (
 			(values (update-octave semitones note-octave))
-			(updated-values (+ note-value (car values)))
 			)
-
+		(let (
+			(updated-values (modded-val (+ note-value (* (list-ref values 0) 100))))
+			(updated-octave (+ note-octave (list-ref values 1)))
+			)
+		(let (
+			(transposed-note (get-note updated-values))
+			)
 		(displaym "values" values)
 		(displaym "updated-values" updated-values)
-		)
-
-		; (let(
-		; 	(vals (update-octave semitones note-octave))
-		; 	(update-note (+ note-value (car (vals))));(2 1)
-		; 	)
-		; (displaym "Updated value:" update-note)
-		; )
-		
-
+		(displaym "new-note" transposed-note)
+		(displaym "new-octave" updated-octave) 
+		)))
 	)
 )
