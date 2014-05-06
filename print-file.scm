f5fd65971b824a2d447d4bad13a3392386e62b7b
Dhruv Parthasarathy <dhruvp@mit.edu>
2014-05-05 21:56:58 -0400

    more printing

--- print-file.scm
+++ print-file.scm
@@ -48,3 +48,44 @@ note
 ("b" 2)
 
 
+(get-scale "Bb" major-tones)
+Value: ("Bb" "C" "D" "Eb" "F" "G" "A")
+
+(get-scale "G#" locrian-tones)
+Value: ("G#" "A" "B" "C#" "D" "E" "F#")
+
+(get-scale "Ebb" dorian-tones)
+Value: ("Ebb" "Fb" "Gbb" "Abb" "Bbb" "Cb" "Dbb")
+
+(get-scale "F" minor-tones)
+Value: ("F" "G" "Ab" "Bb" "C" "Db" "Eb")
+
+(get-scale "C" lydian-tones)
+Value: ("C" "D" "E" "F#" "G" "A" "B")
+
+
+(define (convert-note note-str piece)
+  (let ((newnote (hash-table/get (piece 'get 'table) (get-value note-str) "null")))
+    (if (not (equal? newnote "null"))
+      newnote
+      note-str)))
+
+(define (fill-cent-notes-table)
+  (for-each
+    (lambda (note)
+      (hash-table/put! cent-notes-table (get-value note) note))
+    (get-scale (get-key piece) major-tones))
+)
+
+(convert-note "D#" piece5)
+Value: "Eb"
+
+(convert-note "A#" piece5)
+Value: "Bb"
+
+(convert-note "F#" piece5)
+Value: "F#"
+
+
+
+
